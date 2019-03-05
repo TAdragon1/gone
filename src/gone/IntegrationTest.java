9c51f3f53452780d0a5a99c2c769a19f399ac2fd
TAdragon1 <twa16@case.edu>
2019-03-05 13:58:08 -0500

    Added another test which should end with no black pebbles remaining
    Signed-off-by: TAdragon1 <twa16@case.edu>

--- src/gone/IntegrationTest.java
+++ src/gone/IntegrationTest.java
@@ -3,8 +3,8 @@ package gone.src.gone;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 public class IntegrationTest {
 	
@@ -26,10 +26,46 @@ public class IntegrationTest {
 
 		// Test
 		GameResults testResults = testGame.finalGameResults();
+		int rounds = testResults.getRounds();
+		boolean blackRemaining = testResults.isBlackPebbleRemaining();
 
 		// Assert
-		assertEquals(3, testResults.getRounds());
-		assertTrue(testResults.isBlackPebbleRemaining());
+		System.out.println("Actual rounds = " + rounds);
+		System.out.println("Actual black pebble remaining = " + blackRemaining);
+
+		assertEquals(3, rounds);
+		assertTrue(blackRemaining);
+	}
+
+	@Test
+	public void testLineBlackPebblesOneWhite(){
+		// Setup
+		Map<Coordinate, PebbleColor> testMap = new HashMap<>();
+		testMap.put(new Coordinate(0, 0), PebbleColor.WHITE);
+		testMap.put(new Coordinate(1, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(2, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(3, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(4, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(5, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(6, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(7, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(8, 0), PebbleColor.BLACK);
+		testMap.put(new Coordinate(9, 0), PebbleColor.BLACK);
+
+		BoardConfiguration testConfiguration = new BoardConfiguration(testMap);
+		Gone testGame = new Gone(testConfiguration);
+
+		// Test
+		GameResults testResults = testGame.finalGameResults();
+		int rounds = testResults.getRounds();
+		boolean blackRemaining = testResults.isBlackPebbleRemaining();
+
+		// Assert
+		System.out.println("Actual rounds = " + rounds);
+		System.out.println("Actual black pebble remaining = " + blackRemaining);
+
+		assertEquals(9, rounds);
+		assertFalse(blackRemaining);
 	}
 
 }
